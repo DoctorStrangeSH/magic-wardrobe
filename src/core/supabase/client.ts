859fac88ea9983e847734d8e35e6a0617c6f1ae2
Vladislav Shteinardt <sherlockds@yandex-team.ru>
2026-08-06 12:38:05 +0300

    Обновление

--- src/core/supabase/client.ts
+++ src/core/supabase/client.ts
@@ -1,6 +1,6 @@
 import { createClient } from '@supabase/supabase-js'
 
-const supabaseUrl = 'https://7slqozeltertgiw3derl4a.supabase.co'
+const supabaseUrl = 'https://ykenhwmyyqfnqhqnevzm.supabase.co'
 const supabaseAnonKey = 'sb_publishable_7sLQOZELteRTgiw3dErl4A_rfQIhCEy'
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey)

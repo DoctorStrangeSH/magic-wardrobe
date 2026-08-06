292a909e0694f383bc9dd2dfa8f4ca3ae18720d9
Vladislav Shteinardt <sherlockds@yandex-team.ru>
2026-08-06 12:27:33 +0300

    🔧 v 0.71

--- src/core/supabase/client.ts
+++ src/core/supabase/client.ts
@@ -1,10 +1,6 @@
 import { createClient } from '@supabase/supabase-js'
 
-const supabaseUrl = import.meta.env.VITE_SUPABASE_URL
-const supabaseAnonKey = import.meta.env.VITE_SUPABASE_ANON_KEY
-
-if (!supabaseUrl || !supabaseAnonKey) {
-  throw new Error('Отсутствуют VITE_SUPABASE_URL или VITE_SUPABASE_ANON_KEY в .env')
-}
+const supabaseUrl = 'https://7slqozeltertgiw3derl4a.supabase.co'
+const supabaseAnonKey = 'sb_publishable_7sLQOZELteRTgiw3dErl4A_rfQIhCEy'
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey)
